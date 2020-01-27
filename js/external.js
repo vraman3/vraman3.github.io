c2ebda7705ad301a59982cc3f1c861bde0476aaa
vraman3 <vishwanath.raman3@gmail.com>
2020-01-28 01:45:16 +0530

    Added js code. Any of the onClick() functions are not detected. Chaning to using event listeners which is supposedly better practice.
    From SO: If we use the onClick html attribute, we need to define that
    onClick() within same html elment (by using tags).
    It is best practice to create click event listener using JS/jQuery than
    using onClick() html attribute.

--- js/external.js
+++ js/external.js
@@ -34,15 +34,52 @@ $(document).ready(function() {
         $('.content-div:not('+ currentAttributeValue +')').removeClass('showing').addClass('hiding');
     })
 
-    $("#slideshow > div:gt(0)").hide();
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slidesClass = document.getElementsByClassName("slides");
+        var dot = document.getElementsByClassName("dots");
+        
+        if(n > slidesClass.length) {
+            slideIndex = 1;
+        }
+        if(n < 1) {
+            slideIndex = slidesClass.length;
+        }
+
+        for(i=0; i < slidesClass.length; i++) {
+            slidesClass[i].style.display = "none";
+        }
+        for(i=0; i < dot.length; i++) {
+            dot[i].className = dot[i].className.replace("active", "");
+        }
+
+        slidesClass[slideIndex-1].style.display = "block";
+        dot[slideIndex-1].className += "active";
+
+    }
+
+    /*$("#slideshow > div:gt(0)").hide();
 
     setInterval(function() {
     $('#slideshow > div:first')
-        .fadeOut(1000)
+        .fadeOut(1)
         .next()
-        .fadeIn(1000)
+        .fadeIn(1)
         .end()
         .appendTo('#slideshow');
-    }, 3000);
+    }, 3000); */
 });
 
