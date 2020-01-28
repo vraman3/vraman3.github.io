795b79d0d503a6adf6df52fa6c27f5d7aef6f8e3
vraman3 <vishwanath.raman3@gmail.com>
2020-01-29 00:28:34 +0530

    Added functionality to try new method of slider. Quite a few bugs. Revert to earlier commit and modify that JS logic.

--- js/external.js
+++ js/external.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+$(document).ready(function () {
 
     /*$(".tab-welcome").click(function() {
     
@@ -17,30 +17,139 @@ $(document).ready(function() {
         $("#portfolio").toggle();
     
     });*/
-       
-    $('.tab-item').click(function() {
+
+    $('.tab-item').click(function () {
 
         var currentAttributeValue = $(this).attr('href');
 
         var currentItem = $(currentAttributeValue);
 
-        if(currentItem.hasClass('showing')){
+        if (currentItem.hasClass('showing')) {
             currentItem.removeClass('showing').addClass('hiding');
         }
-        else{
+        else {
             currentItem.removeClass('hiding').addClass('showing');
         }
 
-        $('.content-div:not('+ currentAttributeValue +')').removeClass('showing').addClass('hiding');
+        $('.content-div:not(' + currentAttributeValue + ')').removeClass('showing').addClass('hiding');
     })
 
-    var slideIndex = 1;
+    //$(".left-arrow").hide();
+    var numImgs = $('div.carousel-image-holder img').length;
+
+    var addId = numImgs;
+
+
+
+    if (numImgs == 2) {
+        var clicked = 0;
+        imgCount = numImgs - 2;
+    } else if (numImgs <= 1) {
+        $(".right-arrow").hide();
+    } else {
+        var clicked = 1;
+        imgCount = numImgs - 1;
+    }
+    if (numImgs > 2) {
+        var ul = document.getElementById("carousel-dots");
+
+        for (var i = 0; i < numImgs; i++) {
+            var li = document.createElement("li");
+            li.setAttribute("class", "carousel-buttons");
+            li.setAttribute("id", "carousel" + addId);
+
+            ul.appendChild(li);
+            //$("ul carousel-dots").prepend('<li class="carousel-buttons" id="carousel' + addId + '"></li>');
+            var addId = addId - 1;
+        }
+    } else {
+
+    }
+
+    
+    $(".carousel-buttons").click(function () {
+
+        var findIdClicked = $(this).attr("id");
+
+        var splitString = findIdClicked.split("carousel")
+        var findTheNumb = splitString[1];
+
+        $(".carousel-buttons").removeClass("active");
+        $(this).addClass("active");
+        clicked = parseInt(findTheNumb);
+        var adjustNumberforSlide = findTheNumb - 1;
+
+        $(".carousel-image-holder").animate({ "left": -(600 * adjustNumberforSlide) + "px" });
+        console.log(clicked);
+
+        if (findTheNumb == 1) {
+            $(".left-arrow").hide();
+            $(".right-arrow").show();
+        } else if (findTheNumb == numImgs) {
+            $(".right-arrow").hide();
+            $(".left-arrow").show();
+        } else {
+            $(".right-arrow").show();
+            $(".left-arrow").show();
+        }
+    });
+
+
+    $(".carousel-buttons-container").find("li").first().addClass("active");
+    firstImg = document.getElementsByClassName("carousel-image-holder");
+    firstImg[1].style.display = "block";
+    
+    $(".right-arrow").click(function () {
+
+        if (clicked < imgCount) {
+
+            $(".carousel-image-holder").animate({ "left": "-=600px" });
+
+
+            console.log(clicked);
+        } else {
+            $(".carousel-image-holder").animate({ "left": "-=600px" });
+            $(".right-arrow").hide();
+
+            console.log(clicked);
+        }
+
+        clicked = clicked + 1;
+        $(".left-arrow").show();
+        $(".carousel-buttons").removeClass("active");
+        $("#carousel" + clicked).addClass("active");
+
+    });
+
+    $(".left-arrow").click(function () {
+
+        if (clicked > 2) {
+
+            $(".carousel-image-holder").animate({ "left": "+=600px" });
+
+            console.log(clicked);
+        } else {
+            $(".carousel-image-holder").animate({ "left": "+=600px" });
+            $(".left-arrow").hide();
+
+            console.log(clicked);
+        }
+
+        $(".right-arrow").show();
+        clicked = clicked - 1;
+        $(".carousel-buttons").removeClass("active");
+        $("#carousel" + clicked).addClass("active");
+
+
+    });
+
+    /*var slideIndex = 1;
     showSlides(slideIndex);
 
     $('.prev').click(currentSlide(-1));
     $('#nextSlide').click(currentSlide(1));
 
-    $('.dots').click(currentSlide(1));
+    $('.dotsa').click(currentSlide(2));
 
     // Next/previous controls
     function plusSlides(n) {
@@ -55,7 +164,7 @@ $(document).ready(function() {
     function showSlides(n) {
         var i;
         var slidesClass = document.getElementsByClassName("slides");
-        var dot = document.getElementsByClassName("dots");
+        var dot = document.getElementsByClassName("dotsa");
         
         if(n > slidesClass.length) {
             slideIndex = 1;
@@ -72,9 +181,9 @@ $(document).ready(function() {
         }
 
         slidesClass[slideIndex-1].style.display = "block";
-        /*dot[slideIndex-1].className += "active";*/
+        dot[slideIndex-1].className += "active";
 
-    }
+    }*/
 
     /*$("#slideshow > div:gt(0)").hide();
 
